76ce76a4192f59eb5c77edd799753492dd26dee9
artezan <artezan_015@hotmail.com>
2018-11-21 17:22:28 -0600

    keys

--- src/app/_config/url.api.ts
+++ src/app/_config/url.api.ts
@@ -1,1 +1,3 @@
-export const URL_SERVER = 'http://theartezan.xyz/wp-json';
+export const URL_SERVER = 'https://grupoarza.com/wp-json';
+export const KEYS_WOO =
+  'consumer_key=ck_d55c4038aa856ea8b6fc1fb714bc3462ff798db0&consumer_secret=cs_1ea7c27c92c44401f61835bed8e18d72755b70fb';
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { URL_SERVER } from '../_config/url.api';
+import { URL_SERVER, KEYS_WOO } from '../_config/url.api';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
   constructor(private http: HttpClient) {}
   postProducts(data: any[], token): Observable<any> {
     const headers = new HttpHeaders({
       Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json;'
+      'Content-Type': 'application/json;',
     });
     return this.http.post(
-      URL_SERVER + '/wc/v2/products/batch',
+      URL_SERVER + '/wc/v2/products/batch/?' + KEYS_WOO,
       { create: data },
       {
-        headers: headers
-      }
+        headers: headers,
+      },
     );
   }
 }
